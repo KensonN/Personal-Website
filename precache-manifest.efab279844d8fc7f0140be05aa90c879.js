a6db530a0ce993e1e7a16d483cb61f056d2fb0fc
KensonN <kenson.nguyen3@gmail.com>
2020-09-26 01:01:20 -0700

    Updates

---
+++ precache-manifest.efab279844d8fc7f0140be05aa90c879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe607925ff49b83fe58c5fb4e022baa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e6345900df5f3723c5a",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "49d0a333017731364710",
+    "url": "/static/js/2.20bb0533.chunk.js"
+  },
+  {
+    "revision": "afcffdf4ebf1780e0970711996cdd7dd",
+    "url": "/static/js/2.20bb0533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6345900df5f3723c5a",
+    "url": "/static/js/main.b5a12709.chunk.js"
+  },
+  {
+    "revision": "062e1ed8f07d9d8b9042",
+    "url": "/static/js/runtime-main.ee62f639.js"
+  }
+]);
