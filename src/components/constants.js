e6207daec9f73cf4d3442a98b7a85af723c7d3fd
KensonN <kenson.nguyen3@gmail.com>
2020-09-27 02:35:29 -0700

    Spellcheck.

--- src/components/constants.js
+++ src/components/constants.js
@@ -2,21 +2,21 @@ export default [
     {
         title: "SMV DAQ System and Dashboard",
         skills: "Arduino, LattePanda, Python, ReactJS, Firebase",
-        description: "I worked with other team members to design a modular data-acquisition system including a variety of sensors such as Hall-Effect and gyroscope to track data on electric car to improve efficiency. Additionally, I designed the interface of the driver dashboard inside the car to display the DAQ data to further improve runs as driver previously received information through radio communication. I further iterated on the ground team's dashboard to accomodate for new sensors and improved data display. Both dashboards are toggleable on one page.",
+        description: "I worked with other team members to design a modular data-acquisition system including a variety of sensors such as Hall-Effect and gyroscope to track data on electric car to improve efficiency. Additionally, I designed the interface of the driver dashboard inside the car to display the DAQ data to further improve runs as the driver previously only received information through radio communication. I further iterated on the ground team's dashboard to accommodate for new sensors and improved data display. Both dashboards hosted on one page with a button to toggle between both.",
         imgPath: "dash.gif",
         date: "January 2020 - Present"
     },
     {
         title: "Bluetooth Speaker with RGB Matrix",
         skills: "Arduino, C++",
-        description: "I asssembled a speaker using schematics found online. The speaker hardware consisted of two 3.5\" tweeters connected to a BlueTooth capable audio board. The speaker included an electret microphone connected to an Arduino Mega to sample the audio of the music being played. The audio sample was then processed and converted into audio-reactive lighting effects. These effects were displayed on the 16x16 individually addressable LED matrix affixed to the front of the speaker enclosure. The enclosure was constructed using plywood and MDF.",
+        description: "I assembled a speaker using schematics found online. The speaker hardware consisted of two 3.5\" tweeters connected to a BlueTooth capable audio board. The speaker included an electret microphone connected to an Arduino Mega to sample the audio of the music being played. The audio sample was then processed and converted into audio-reactive lighting effects. These effects were displayed on the 16x16 individually addressable LED matrix affixed to the front of the speaker enclosure. The enclosure was constructed using plywood and MDF.",
         imgPath: "speaker.jpg",
         date: "June - July 2018"
     },
     {
         title: "Logitech G910 Lighting for Music Shows",
         skills: "C++",
-        description: "As part of junior year project, I learned and wrote a C++ program to control the LEDS on the Logitech G910 keyboard. The keyboard has an individually addressable RGB LED under each key which can be programmed by the Logitech Lighting SDK. I wrote a program that generated lighting effects upon the press of certain keys, inspired by how visual jockeys manually control stage effects and visuals at electronic music shows. ",
+        description: "As part of junior year project, I learned and wrote a C++ program to control the LEDS on the Logitech G910 keyboard. The keyboard has an individually addressable RGB LED under each key which can be programmed by the Logitech Lighting SDK. I wrote a program that generated lighting effects upon the press of certain keys, inspired by how visual jockeys manually control stage effects and visuals at electronic music shows.",
         imgPath: "genius.gif",
         date: "February 2017",
     },
